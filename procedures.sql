5c1fdbdad427c568930267fe45ba6b6194917c70
Oran Can Ören <orancanoren@gmail.com>
2017-04-12 00:15:32 +0300

    Update procedures.sql

--- procedures.sql
+++ procedures.sql
@@ -75,3 +75,17 @@ BEGIN
 	DELETE FROM `employees` WHERE `ssn` = ssn;
 END $$
 -- berkyaglioglu end
+
+-- Bahadır Yurtkulu Begin
+CREATE PROCEDURE cancelFlightbyPassenger(customerNoEntered VARCHAR(11), flightNumEntered CHAR(7))
+BEGIN
+
+DELETE FROM flies WHERE customer_no = customerNoEntered AND flight_number = flightNumEntered;
+
+COMMIT;
+
+DELETE FROM passengers
+WHERE passengers.customer_no NOT IN (SELECT customer_no FROM flies);
+
+END $$
+-- Bahadır Yurtkulu End
