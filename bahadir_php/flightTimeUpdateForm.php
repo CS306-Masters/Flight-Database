abcb6943e255d3ca98b0eb21b3cd9823d7a0909d
orancanoren <orancanoren@gmail.com>
2017-08-21 23:30:23 +0300

    web application added

--- bahadir_php/flightTimeUpdateForm.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<body>
-<form method="post" action="flightTimeUpdate.php">
-	Flight Number: <input type="text" name="flightNumber"><br>
-	New Time: <input type="text" name="newTime"><br>
-	<input type="submit" value="Update">
-</body>
-</html>
--- bahadir_php/removeFlightForm.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<body>
-<form method="post" action="removeFlight.php">
-	Customer No*: <input type="text" name="customerNumber"><br>
-	Flight Number*: <input type="text" name="flightNumber"><br>
-	<input type="submit" value="remove flight">
-</body>
-</html>
